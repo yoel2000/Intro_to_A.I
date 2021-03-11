9a1c1fdea747b96a4adf26c1329a41c6dae497ed
yoelb <yoel.bensoussan@gmail.com>
2021-03-11 15:06:54 +0200

    commencer ex2

--- Ex1/search.py
+++ Ex1/search.py
@@ -1,22 +1,23 @@
 
 
 import state
-import frontier
+from Ex1 import frontier
+
 
 def search(n):
 
-        s=state.create(n)
+        s= state.create(n)
         # print(s)
-        f=frontier.create(s)
+        f= frontier.create(s)
 
         while not frontier.is_empty(f):
-            s=frontier.remove(f)
+            s= frontier.remove(f)
             if state.is_target(s):
               return [f[1], f[3]]
-            ns=state.get_next(s)
+            ns= state.get_next(s)
             #print(ns)
             for i in ns:
-                frontier.insert(f,i)
+                frontier.insert(f, i)
         return 0
 
 max_depth=0
