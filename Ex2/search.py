17e5a4263a8ef465d46edb5ca031fec66bad0b72
yoelb <yoel.bensoussan@gmail.com>
2021-03-22 18:49:07 +0200

    manhattan distance

--- Ex2/search.py
+++ Ex2/search.py
@@ -1,21 +1,42 @@
-#search
 import state
 import frontier
 
+
 def search(n):
-    s=state.create(n)
+    s = state.create(n)
     print(s)
-    f=frontier.create(s)
+    f = frontier.create(s)
     while not frontier.is_empty(f):
-        s=frontier.remove(f)
+        s = frontier.remove(f)
         if state.is_target(s):
-            return [s, f[1], f[3]]
-        ns=state.get_next(s)
+            return [len(s[1]), f[2]]
+        ns = state.get_next(s)
         for i in ns:
-            frontier.insert(f,i)
+            frontier.insert(f, i)
     return 0
 
 
-print (search(3))
+max_depth=0
+sum_item=0
+
+print("search(3):")
+for i in range(100):
+    x = search(3)
+    max_depth += x[0]
+    sum_item += x[1]
+print("Average depth", max_depth/100)
+print("Average items", sum_item/100)
 
 
+max_depth=0
+sum_item=0
+
+# print("search(4):")
+# for i in range(10):
+#     x = search(4)
+#     max_depth += x[0]
+#     sum_item += x[1]
+# print("Average depth", max_depth/10)
+# print("Average items", sum_item/10)
+
+# print(search(3))
--- Ex2/state.py
+++ Ex2/state.py
@@ -9,57 +9,83 @@ The target for 8-puzzle is: (zero is the hole)
 import random
 import math
 
-#returns a random board nXn
-def create(n):
-    s=list(range(n*n))      # s is the board itself. a vector that represent a matrix. s=[0,1,2....n^2-1]
-    m="<>v^"                # m is "<>v^" - for every possible move (left, right, down, up)
-    for i in range(n**3):  # makes n^3 random moves to mix the tiles
-        if_legal(s,m[random.randrange(4)])
-    return [s,""]           # at the beginning "" is an empty path, later on path
-                            # contains the path that leads from the initial state to the state
 
-def get_next(x):            # returns a list of the children states of x
-    ns=[]                   # the next state list
+# returns a random board nXn
+def create(n):
+    s = list(range(n * n))  # s is the board itself. a vector that represent a matrix. s=[0,1,2....n^2-1]
+    m = "<>v^"  # m is "<>v^" - for every possible move (left, right, down, up)
+    for i in range(n ** 3):  # makes n^3 random moves to mix the tiles
+        if_legal(s, m[random.randrange(4)])
+    return [s, ""]  # at the beginning "" is an empty path, later on path
+    # contains the path that leads from the initial state to the state
+
+
+def get_next(x):  # returns a list of the children states of x
+    ns = []  # the next state list
     for i in "<>v^":
-        s=x[0][:]           # [:] - copies the board in x[0]
-        if_legal(s,i)       # try to move in direction i
+        s = x[0][:]  # [:] - copies the board in x[0]
+        if_legal(s, i)  # try to move in direction i
         # checks if the move was legal and...
-        if s.index(0)!=x[0].index(0) and \
-           (x[1]=="" or x[1][-1]!="><^v"["<>v^".index(i)]): # check if it's the first move or it's a reverse move
-            ns.append([s,x[1]+i])   # appends the new state to ns
+        if s.index(0) != x[0].index(0) and \
+                (x[1] == "" or x[1][-1] != "><^v"[
+                    "<>v^".index(i)]):  # check if it's the first move or it's a reverse move
+            ns.append([s, x[1] + i])  # appends the new state to ns
     return ns
 
 
 def path_len(x):
     return len(x[1])
 
+
 def is_target(x):
-    n=len(x[0])                     # the size of the board
-    return x[0]==list(range(n))     # list(range(n)) is the target state
+    n = len(x[0])  # the size of the board
+    return x[0] == list(range(n))  # list(range(n)) is the target state
+
 
 #############################
-def if_legal(x,m):                  # gets a board and a move and makes the move if it's legal
-    n=int(math.sqrt(len(x)))        # the size of the board is nXn
-    z=x.index(0)                    # z is the place of the empty tile (0)
-    if z%n>0 and m=="<":            # checks if the empty tile is not in the first col and the move is to the left
-        x[z]=x[z-1]                 # swap x[z] and x[z-1]...
-        x[z-1]=0                    # ...and move the empty tile to the left
-    elif z%n<n-1 and m==">":        # check if the empty tile is not in the n's col and the move is to the right
-        x[z]=x[z+1]
-        x[z+1]=0
-    elif z>=n and m=="^":           # check if the empty tile is not in the first row and the move is up
-        x[z]=x[z-n]
-        x[z-n]=0
-    elif z<n*n-n and m=="v":        # check if the empty tile is not in the n's row and the move is down
-        x[z]=x[z+n]
-        x[z+n]=0
+def if_legal(x, m):  # gets a board and a move and makes the move if it's legal
+    n = int(math.sqrt(len(x)))  # the size of the board is nXn
+    z = x.index(0)  # z is the place of the empty tile (0)
+    if z % n > 0 and m == "<":  # checks if the empty tile is not in the first col and the move is to the left
+        x[z] = x[z - 1]  # swap x[z] and x[z-1]...
+        x[z - 1] = 0  # ...and move the empty tile to the left
+    elif z % n < n - 1 and m == ">":  # check if the empty tile is not in the n's col and the move is to the right
+        x[z] = x[z + 1]
+        x[z + 1] = 0
+    elif z >= n and m == "^":  # check if the empty tile is not in the first row and the move is up
+        x[z] = x[z - n]
+        x[z - n] = 0
+    elif z < n * n - n and m == "v":  # check if the empty tile is not in the n's row and the move is down
+        x[z] = x[z + n]
+        x[z + n] = 0
+
 
 # This is your HW
-def hdistance(s):                   # the heuristic value of s -- uniform cost
+def hdistance(s):  # the heuristic value of s -- uniform cost
     return 0
 
-def hdistance1(s):                   # for h1 (tiles out of place)
+
+# question 1
+def hdistance1(s):  # for h1 (tiles out of place)
+    state = s[0]
+    counter = 0
+    for i in state:
+        if state[i] != 0:
+            if i != state[i]:
+                counter += 1
+    # print(state)
+    # print(counter)
     return 0
 
-def hdistance2(s):                   # for h2 (Manhattan) 
-    return 0
+
+def hdistance2(s):  # for h2 (Manhattan)
+    initial_state = s[0]
+    initial_config = initial_state
+    manDict = 0
+    for i,item in enumerate(initial_config):
+        prev_row,prev_col = int(i/ 3) , i % 3
+        goal_row,goal_col = int(item /3),item % 3
+        manDict += abs(prev_row-goal_row) + abs(prev_col - goal_col)
+    print(manDict)
+    return manDict
+
